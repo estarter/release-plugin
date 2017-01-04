6b2148d0e6f9444f610ab3b28167aac9432022f0
Alexey Merezhin <amerezhin@e3ag.ch>
2017-01-04 16:20:33 +0100

    add first test

---
+++ src/test/java/hudson/plugins/release/pipeline/ReleaseStepTest.java
@@ -0,0 +1,52 @@
+package hudson.plugins.release.pipeline;
+
+import java.util.List;
+
+import org.jenkinsci.plugins.workflow.cps.CpsFlowDefinition;
+import org.jenkinsci.plugins.workflow.job.WorkflowJob;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+import org.jvnet.hudson.test.BuildWatcher;
+import org.jvnet.hudson.test.Issue;
+import org.jvnet.hudson.test.JenkinsRule;
+import org.jvnet.hudson.test.LoggerRule;
+import org.jvnet.hudson.test.TestExtension;
+import org.jvnet.hudson.test.recipes.LocalData;
+
+import hudson.model.Action;
+import hudson.model.FreeStyleProject;
+import hudson.model.Queue;
+
+/**
+ * Created by e3cmea on 1/4/17.
+ *
+ * @author Alexey Merezhin
+ */
+public class ReleaseStepTest {
+    @ClassRule
+    public static BuildWatcher buildWatcher = new BuildWatcher();
+    @Rule
+    public JenkinsRule j = new JenkinsRule();
+    @Rule public LoggerRule logging = new LoggerRule();
+
+
+    @Test
+    public void releaseProject() throws Exception {
+        FreeStyleProject ds = j.createFreeStyleProject("ds");
+        WorkflowJob us = j.jenkins.createProject(WorkflowJob.class, "us");
+        us.setDefinition(new CpsFlowDefinition("release 'ds'\n echo \"release's done\"", true));
+
+        // Jan 04, 2017 4:10:26 PM hudson.ExtensionFinder$GuiceFinder$SezpozModule configure
+        // WARNING: Failed to load hudson.ivy.builder.AntIvyBuilderType$DescriptorImpl
+        // java.lang.NoClassDefFoundError: [Lhudson/tasks/Ant$AntInstallation;
+        // WARNING: Failed to instantiate optional component org.jenkinsci.plugins.workflow.steps.scm.SubversionStep$DescriptorImpl; skipping
+        // Stacktrace: org.jenkinsci.plugins.workflow.steps.MissingContextVariableException: Required context class hudson.Launcher is missing
+
+        j.assertLogContains("release's done", j.buildAndAssertSuccess(us));
+        // TODO JENKINS-28673 assert no warnings, as in StartupTest.noWarnings
+        // (but first need to deal with `WARNING: Failed to instantiate optional component org.jenkinsci.plugins.workflow.steps.scm.SubversionStep$DescriptorImpl; skipping`)
+        ds.getBuildByNumber(1).delete();
+    }
+
+}
