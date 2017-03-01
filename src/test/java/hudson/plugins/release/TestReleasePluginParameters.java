43a0862dcea33798c4f1693e2a30762d70a35f8b
Alexey Merezhin <amerezhin@e3ag.ch>
2017-03-01 19:59:29 +0100

    add testReleaseMixedParameters - works in test env but fails in real

--- src/test/java/hudson/plugins/release/TestReleasePluginParameters.java
+++ src/test/java/hudson/plugins/release/TestReleasePluginParameters.java
@@ -1,9 +1,15 @@
 package hudson.plugins.release;
 
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
 
 import java.io.IOException;
+import java.io.Reader;
 import java.util.Arrays;
+import java.util.Scanner;
 
 import org.junit.Rule;
 import org.junit.Test;
@@ -15,6 +21,7 @@ import com.gargoylesoftware.htmlunit.html.HtmlForm;
 
 import hudson.Launcher;
 import hudson.model.AbstractBuild;
+import hudson.model.BooleanParameterDefinition;
 import hudson.model.BuildListener;
 import hudson.model.FreeStyleProject;
 import hudson.model.Job;
@@ -23,6 +30,7 @@ import hudson.model.ParametersDefinitionProperty;
 import hudson.model.Result;
 import hudson.model.StringParameterDefinition;
 import hudson.tasks.BuildStep;
+import hudson.tasks.Shell;
 
 public class TestReleasePluginParameters {
 
@@ -53,6 +61,42 @@ public class TestReleasePluginParameters {
     }
 
     @Test
+    public void testReleaseMixedParameters() throws Exception {
+        FreeStyleProject proj = j.createProject(FreeStyleProject.class, "foo");
+
+        ReleaseWrapper releaseWrapper = new ReleaseWrapper();
+        releaseWrapper.setParameterDefinitions(Arrays.asList(new ParameterDefinition [] {
+                new StringParameterDefinition("version", "auto"),
+                new BooleanParameterDefinition("dryRun", true, "description")}));
+
+        CheckerBuildStep checkerBuildStepVersion = new CheckerBuildStep("version");
+        CheckerBuildStep checkerBuildStepDryRun = new CheckerBuildStep("dryRun");
+        releaseWrapper.setPostSuccessfulBuildSteps(
+                Arrays.asList(new BuildStep [] {
+                        checkerBuildStepVersion,
+                        checkerBuildStepDryRun,
+                        new Shell("echo $version\necho $dryRun")
+                }));
+        proj.getBuildWrappersList().add(releaseWrapper);
+
+        scheduleReleaseBuild(proj);
+
+        j.waitUntilNoActivity();
+        assertTrue("Build finishes", proj.getLastBuild() != null);
+
+        assertNotNull("The parameter must be visible as environment variable", checkerBuildStepVersion.value);
+        assertEquals("The parameter value must match", "auto", checkerBuildStepVersion.value);
+        assertNotNull("The parameter must be visible as environment variable", checkerBuildStepDryRun.value);
+        assertEquals("The parameter value must match", "true", checkerBuildStepDryRun.value);
+        j.assertBuildStatus(Result.SUCCESS, proj.getLastBuild());
+
+        Scanner scanner = new Scanner(proj.getLastBuild().getLogInputStream()).useDelimiter("\\A");
+        String log = scanner.next();
+        assertThat(log, containsString("echo auto"));
+        assertThat(log, containsString("echo true"));
+    }
+
+    @Test
     @Issue("JENKINS-34996") // This test worked before the fix, added just as a verification
     public void testJobParameters() throws Exception {
         FreeStyleProject prj = j.createProject(FreeStyleProject.class, "foo");
@@ -74,17 +118,23 @@ public class TestReleasePluginParameters {
     }
 
     public static class CheckerBuildStep extends MockBuilder {
-
+        private String key;
         public String value;
 
         public CheckerBuildStep() {
             super(Result.SUCCESS);
+            this.key = "TEST";
+        }
+
+        public CheckerBuildStep(String key) {
+            super(Result.SUCCESS);
+            this.key = key;
         }
 
         @Override
         public boolean perform(AbstractBuild<?, ?> build, Launcher launcher, BuildListener listener)
                 throws InterruptedException, IOException {
-            value = build.getEnvironment(listener).get("TEST");
+            value = build.getEnvironment(listener).get(key);
             return true;
         }
     }
